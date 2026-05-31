4af8d0a3756e13efe3a665baaf4df4c30ac46658
Patrick Passos <patrickpassosb@gmail.com>
2026-05-30 21:38:09 -0300

    feat(zsh): add zsh autosuggest and syntax highlighting
    update the zshrc to enable these two plugins, rewrite the oh-my-zsh install script to only run the official installer when omz is not already present, and add automatic installation of the custom plugins

--- installers/oh-my-zsh.sh
+++ installers/oh-my-zsh.sh
@@ -4,14 +4,29 @@ set -euo pipefail
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/../lib/helpers.sh"
 
-if [ -d "$HOME/.oh-my-zsh" ]; then
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  require_cmd zsh
+  log "Installing Oh My Zsh..."
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+  log "Oh My Zsh installed."
+else
   log "Oh My Zsh is already installed at $HOME/.oh-my-zsh"
-  exit 0
 fi
 
-require_cmd zsh
+# Install custom plugins
+ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+mkdir -p "$ZSH_CUSTOM/plugins"
 
-log "Installing Oh My Zsh..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
+  log "Installing ZSH Syntax Highlighting..."
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+else
+  log "ZSH Syntax Highlighting is already installed."
+fi
 
-log "Oh My Zsh installed."
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-autosuggestions" ]; then
+  log "Installing ZSH Auto Suggestions..."
+  git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+else
+  log "ZSH Auto Suggestions is already installed."
+fi
